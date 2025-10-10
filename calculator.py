d58863e2257480c4f0e087f77dad5dd28b1c0daf
LemonIce52 <128214825+LemonIce52@users.noreply.github.com>
2025-10-10 10:44:55 +0300

    first commit

---
+++ calculator.py
@@ -0,0 +1,45 @@
+class Calculator:
+
+    def add(self, a: float, b: float) -> float:
+        """This method adding two numbers (a and b)"""
+        return a + b
+
+    def subtract(self, a: float, b: float) -> float:
+        """This method subtract two numbers (a and b)"""
+        return a - b
+
+    def multiply(self, a: float, b: float) -> float:
+        """This method multiply two numbers (a and b)"""
+        return a * b
+
+    def divided(self, a: float, b: float) -> float:
+        """This method divided a in b if b not equals zero"""
+        if b == 0:
+            return 0
+        else:
+            return a / b
+
+
+def main() -> None:
+    """This main method, dot run"""
+    signs = ["+", "-", "/", "*"]
+    calculate = Calculator()
+
+    while True:
+        a, b = map(int, input("Please enter two number format (a b): ").split(" "))
+        s = input("Please enter the action sign (action signs +, -, /, *): ")
+        result = 0
+        if s in signs:
+            match s:
+                case "+": result = calculate.add(a, b)
+                case "-": result = calculate.subtract(a, b)
+                case "/": result = calculate.divided(a, b)
+                case "*": result = calculate.multiply(a, b)
+
+            print(f"result = {result}")
+            break
+        else:
+            print("Please enter correct sign!")
+
+if __name__ == "__main__":
+    main()
